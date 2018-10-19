bda17505a18e52a40b5fb9d0371e2168ae67ebba
Alberto Madrazo <otreblatercero@gmail.com>
2018-10-19 05:24:15 -0500

    Primer commit.

---
+++ src/components/Player.vue
@@ -0,0 +1,33 @@
+<template>
+	<div>
+		<h4>Ahora se escucha</h4>
+		<!-- <h2 :playing="now_playing">{{ nowPlaying }}</h2> -->
+		<h2>Beck - Nausea</h2>
+		<h3>Ahora se escucha</h3>
+		<div>
+			<span>2:11</span>
+			<span>4:27</span>
+		</div>
+		<div></div>
+	</div>
+</template>
+
+<script>
+	
+export default {
+	name: 'Player',
+	data: {
+		playingSong: {
+			artista: 'Beck',
+			cancion: 'Nausea'
+		}
+	}
+}
+
+</script>
+
+<style scoped>
+	
+
+
+</style>
